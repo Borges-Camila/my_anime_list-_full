ebda8c818453b7f6c4fce893f8a030571272490c
Camila Borges de Jesus <camila.borges@lacreisaude.com.br>
2025-05-13 20:24:50 -0300

    Feat: criação dos controllers + Fix: movimentação dos arquivos para a pasta de models

---
+++ backend/controllers/cards.js
@@ -0,0 +1,65 @@
+import CardModel from '../models/card.js';
+
+// busca os cartões
+async function listCards() {
+  try {
+    const cards = await CardModel.find();
+    return cards;
+  } catch (error) {
+    throw new Error(
+      'Não foi possivel encontrar os cartºoes',
+    ).mongoError('Mongo - get cards');
+  }
+}
+
+async function storeCard(items, userId) {
+  try {
+    const { title, titleJapanese, images, synopsis } = items;
+
+    if (!title || !images) {
+      throw new Error('Nome e link são obrigatórios');
+    }
+
+    const newCard = new CardModel({
+      title,
+      titleJapanese,
+      images,
+      synopsis,
+      owner: userId,
+    });
+    const createdCard = await newCard.save();
+
+    return createdCard;
+  } catch (error) {
+    throw new Error('Não foi possível criar o cartão');
+  }
+}
+
+// remove cartão
+async function removeCard(cardId, userId) {
+  try {
+    const card = await CardModel.findById(cardId).orFail(() => {
+      const error = new Error('Cartão não encontrado');
+      error.statusCode = 404;
+      throw error;
+    });
+
+    if (card.owner.toString() !== userId) {
+      throw new Error(
+        'Você não tem permissão para deletar este cartão',
+      );
+    }
+
+    await CardModel.findByIdAndDelete(cardId);
+    return { message: 'Cartão foi deletado com sucesso' };
+  } catch (error) {
+    console.error('Erro ao deletar cartão:', error);
+    throw new Error(
+      error.statusCode === 404
+        ? 'Cartão não encontrado'
+        : 'Não foi possível deletar o cartão',
+    );
+  }
+}
+
+export { listCards, storeCard, removeCard };
